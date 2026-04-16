5be022d3c242810d02a4eb60102e82e6d25eec1b
lucaslucas232 <lucasabalo23@gmail.com>
2026-04-15 22:03:44 -0300

    java

---
+++ ejercicios/variables_y_condicionales/script.js
@@ -0,0 +1,15 @@
+
+let nombre = ""
+if (nombre != "") {
+    console.log("hola, " + nombre)
+} else {
+    console.log("hola, desconocido")
+}
+
+let numero = 7;
+
+if (numero % 2 === 0) {
+    console.log("El número " + numero + " es PAR");
+} else {
+    console.log("El número " + numero + " es IMPAR");
+}
