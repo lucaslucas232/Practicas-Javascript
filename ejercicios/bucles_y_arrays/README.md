3adfd8932f48bb4658a1bd44112d58af46ce39e8
lucaslucas232 <lucasabalo23@gmail.com>
2026-04-20 21:58:00 -0300

    avances

--- ejercicios/bucles_y_arrays/README.md
+++ ejercicios/bucles_y_arrays/README.md
@@ -37,7 +37,9 @@ Dado un array de números, encontrar el número más grande.
 
 ```js
 cost numeros = [4, 17, 3, 25, 8, 11];
-let mayor = numeros[0]; // empezamos asumiendo que el primero es el mayor
+let mayor = numeros[0];
+
+
 
 // agregar el bucle for (usar .length para saber el largo del array)
 
---
+++ ejercicios/bucles_y_arrays/script.js
@@ -0,0 +1,34 @@
+// Ejercicio 1 //
+let suma = 0
+for (suma=1; suma<=55; suma++) {
+    console.log("Suma total: ", suma);
+}
+
+// Ejercicio 2 //
+let contador = 11
+
+while(contador = contador - 1) {
+    console.log(contador)
+}
+
+// Ejercicio 3 //
+const numeros = [4, 17, 3, 25, 8, 11];
+let mayor = numeros[0];
+for (let i = 1; i < numeros.length; i++) {
+    if (numeros[i] > mayor) {
+        mayor = numeros[i];
+    }
+}
+console.log("El número mayor es: ", mayor); // Resultado esperado: 25
+
+// Ejercicio 4 //
+
+const palabras = ["sol", "montaña", "mar", "elefante", "río", "cascada"];
+const palabrasLargas = [];
+
+for (let i = 0; i < palabraslargas.length; i++) {
+    if (palabraslargas.length > 5) { 
+         palabras.push(palabrasLargas) {
+        console.log(palabras)
+    }
+}
