ca04b5c8648b47b58f7c5d15a16eca533f1765af
lucaslucas232 <lucasabalo23@gmail.com>
2026-04-22 22:00:57 -0300

    ejercicios function

--- ejercicios/bucles_y_arrays/script.js
+++ ejercicios/bucles_y_arrays/script.js
@@ -1,23 +1,23 @@
 // Ejercicio 1 //
-let suma = 0
-for (suma=1; suma<=55; suma++) {
-    console.log("Suma total: ", suma);
+let suma = 0;
+for (suma = 1; suma <= 55; suma++) {
+  console.log("Suma total: ", suma);
 }
 
 // Ejercicio 2 //
-let contador = 11
+let contador = 11;
 
-while(contador = contador - 1) {
-    console.log(contador)
+while ((contador = contador - 1)) {
+  console.log(contador);
 }
 
 // Ejercicio 3 //
 const numeros = [4, 17, 3, 25, 8, 11];
 let mayor = numeros[0];
 for (let i = 1; i < numeros.length; i++) {
-    if (numeros[i] > mayor) {
-        mayor = numeros[i];
-    }
+  if (numeros[i] > mayor) {
+    mayor = numeros[i];
+  }
 }
 console.log("El número mayor es: ", mayor); // Resultado esperado: 25
 
@@ -27,22 +27,21 @@ const palabras = ["sol", "montaña", "mar", "elefante", "río", "cascada"];
 const palabrasLargas = [];
 
 for (let i = 0; i < palabras.length; i++) {
-    if (palabras[i].length > 5) { 
-        palabrasLargas.push(palabras[i]);
-    }
+  if (palabras[i].length > 5) {
+    palabrasLargas.push(palabras[i]);
+  }
 }
 
 console.log("Palabras con más de 5 letras:", palabrasLargas);
 
 // ejercicio 5 //
 
-const numero = 7; 
+const numero = 7;
 let multiplicador = 1;
-let resultado = 0
-while (multiplicador <= 10) { 
-    resultado = (numero * multiplicador,multiplicador++)
-  console.log(numero + "x" + multiplicador + "=" + resultado );
+
+while (multiplicador <= 10) {
+  let resultado = 0;
+  resultado = numero * multiplicador;
+  console.log(numero + "x" + multiplicador + "=" + resultado);
+  multiplicador++;
 }
-
-// Ejercicio 6 //
-
--- ejercicios/funciones/script.js
+++ ejercicios/funciones/script.js
@@ -1,1 +1,39 @@
 // Definición de funciones acá
+
+// ejercicio 1 //
+
+function saludar(nombre) {
+    return "Hola, " + nombre + "!"
+    console.log(saludar)
+}
+
+
+// ejercicio 2 //
+
+function calculadora (numero1, numero2, operacion) {
+    if(operacion === "+")
+        return numero1 + numero2;
+    else if(operacion === "-")
+        return numero1 - numero2;
+    else if(operacion === "/")
+        return numero1 / numero2;
+    else if(operacion === "*")
+        return numero1 * numero2; 
+    console.log(calculadora)
+
+}
+
+// ejercicio 3 //
+let areaRectangulo =
+function areaRectangulo (numero1, numero2) {
+    return numero1 * numero2
+    console.log(areaRectangulo)
+}
+// ejercicio 4 //
+let numero = 4;
+
+if (numero % 2 === 0) {
+    console.log("El número " + numero + " es PAR");
+} else {
+    console.log("El número " + numero + " es IMPAR");
+}
