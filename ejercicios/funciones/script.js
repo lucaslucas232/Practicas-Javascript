4ac22fd2893813f9ef16f20ebb771cb757232da5
lucaslucas232 <lucasabalo23@gmail.com>
2026-04-26 12:39:27 -0300

    ejercicio 6

--- ejercicios/funciones/script.js
+++ ejercicios/funciones/script.js
@@ -30,10 +30,24 @@ function areaRectangulo (numero1, numero2) {
     console.log(areaRectangulo)
 }
 // ejercicio 4 //
-let numero = 4;
 
-if (numero % 2 === 0) {
-    console.log("El número " + numero + " es PAR");
-} else {
-    console.log("El número " + numero + " es IMPAR");
+const esPar = (numero) => numero % 2 === 0 ? "es par" : "es impar";
+
+// ejercicio 5 //
+
+const celsiusAFahrenheit = (C) => (C * 9/5) + 32;
+
+
+// ejercicio 6 // 
+function calificarNota(nota) {
+    if (nota >= 90) {
+        return "A";
+    } else if (nota >= 80) {
+        return "B";
+    } else if (nota >= 70) {
+        return "C";
+    } else {
+        return "F";
+    }
 }
+
