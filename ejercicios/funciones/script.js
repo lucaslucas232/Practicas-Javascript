d41b37ce9561c0da586d1132045e3fe844bb1a08
lucaslucas232 <lucasabalo23@gmail.com>
2026-04-29 19:12:58 -0300

    goo

--- ejercicios/funciones/script.js
+++ ejercicios/funciones/script.js
@@ -51,3 +51,27 @@ function calificarNota(nota) {
     }
 }
 
+
+// ejercicio 7 //
+
+function sumarArray(numeros) {
+  let suma = 0;
+  for (let i = 0; i < numeros.length; i++) {
+    suma += numeros[i];
+  }
+  return suma;
+}
+
+console.log(sumarArray([1, 2, 3, 4])); 
+
+// ejercicio 8 //
+
+function sumarArray(numeros) {
+  let suma = 0;
+  for (let i = 0; i < numeros.length; i++) {
+    suma += numeros[i];
+  }
+  return suma;
+}
+
+console.log(sumarArray([1, 2, 3, 4])); 
