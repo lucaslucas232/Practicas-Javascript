d17b06963e46b3225a952dfac237b2ef307c72ce
lucaslucas232 <lucasabalo23@gmail.com>
2026-05-11 21:49:43 -0300

    timeout headache

--- ejercicios/asincronia_y_fetch/fetch.js
+++ ejercicios/asincronia_y_fetch/fetch.js
@@ -1,38 +1,44 @@
 //ejercicio 1//
 
-
 fetch("https://pokeapi.co/api/v2/pokemon/pikachu")
-.then(res => {
-    if(res.ok === false) throw new Error("error");
+  .then((res) => {
+    if (res.ok === false) throw new Error("error");
     return res.json();
-})
-.then((data) => {
+  })
+  .then((data) => {
     console.log(JSON.stringify(data));
-    data.abilities.slice(0, 3).forEach(h => {
-        console.log(h.ability.name);
+    data.abilities.slice(0, 3).forEach((h) => {
+      console.log(h.ability.name);
     });
-})
-.catch((error) => console.error(error));
+  })
+  .catch((error) => console.error(error));
 
-//ejercicio 2 //
-const lista = document.querySelector("#lista-pokemon")
+//ejercicio 2 y 3 //
+const lista = document.querySelector("#lista-pokemon");
+const loading = document.querySelector("#loading");
 
-fetch("https://pokeapi.co/api/v2/pokemon/")
-.then(res => {
-    if(res.ok === false) throw new Error("error");
-    return res.json();
-})
-.then((data) => {
-    const pokemones = data.results;
-    pokemones.forEach(pokemon => {
+setTimeout(() => {
+  fetch("https://pokeapi.co/api/v2/pokemon/")
+    .then((res) => {
+      if (res.ok === false) throw new Error("error");
+      return res.json();
+    })
+    .then((data) => {
+      loading.textContent = "";
+
+      const pokemones = data.results;
+      pokemones.forEach((pokemon) => {
         const li = document.createElement("li");
 
         const anchor = document.createElement("a");
-        anchor.href = pokemon.url; 
+        anchor.href = pokemon.url;
         anchor.textContent = pokemon.name;
 
         li.appendChild(anchor);
         lista.appendChild(li);
-    });
-})
-.catch((error) => console.error(error));
+      });
+    })
+    .catch((error) => console.error(error));
+}, 2000);
+
+// ejercicio 3 //
